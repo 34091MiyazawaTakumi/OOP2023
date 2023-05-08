53e0d3b090a30457e9334e02d7be6d8c33c8e48d
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-05-08 10:54:14 +0900

    P26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+
+    //商品クラス
+    class Program {
+        static void Main(string[] args) {
+            //インスタンスの生成
+            Product Karinto = new Product(123, "かりんとう", 180);
+            Product Daifuku = new Product(235, "大福もち", 160);
+
+            Console.WriteLine("税込価格" + Karinto.GetPriceIncludingTax());
+            Console.WriteLine("税込価格" + Daifuku.GetPriceIncludingTax());
+
+        }
+    }
+}
