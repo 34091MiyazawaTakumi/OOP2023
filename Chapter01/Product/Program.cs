80582ef46a8056b746f92e116abb51a9325d1dc5
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-05-08 11:49:59 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace ProductSample {
 
-    //商品クラス
     class Program {
         static void Main(string[] args) {
+
+            #region P26のサンプルプログラム
             //インスタンスの生成
-            Product Karinto = new Product(123, "かりんとう", 180);
-            Product Daifuku = new Product(235, "大福もち", 160);
+            //Product Karinto = new Product(123, "かりんとう", 180);
+            //Product Daifuku = new Product(235, "大福もち", 160);
 
-            Console.WriteLine("税込価格" + Karinto.GetPriceIncludingTax());
-            Console.WriteLine("税込価格" + Daifuku.GetPriceIncludingTax());
+            //Console.WriteLine("税込価格" + Karinto.GetPriceIncludingTax());
+            //Console.WriteLine("税込価格" + Daifuku.GetPriceIncludingTax());
+            #endregion
 
+            //DateTime date = new DateTime(2023, 5, 8);
+            DateTime date = DateTime.Today;
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            //10日前を求める
+            DateTime daysBefore10 = date.AddDays(-10);
+
+            Console.WriteLine("今日の日付:" + date);
+            Console.WriteLine("10日後:" + daysAfter10.ToString(@"yyyy\/MM\/dd"));
+            Console.WriteLine("10日前:" + daysBefore10.ToString(@"yyyy\/MM\/dd"));
         }
     }
 }
