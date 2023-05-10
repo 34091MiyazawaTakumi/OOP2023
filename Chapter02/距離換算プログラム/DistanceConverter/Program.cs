22adb32610893a0ecb9c2016eb6ad6d882fd7fbc
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-05-10 11:04:31 +0900

    静的クラス、静的メソッド化

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-    class Program : FeetConverter{
+    class Program {
         static void Main(string[] args) {
 
             if (args.Length >= 1 && args[0] == "-tom") {
@@ -18,20 +18,16 @@ namespace DistanceConverter {
 
         //フィートからメートルへの対応表を出力
         private static void PrintFeetToMeterList(int start, int stop) {
-            FeetConverter toMeter = new FeetConverter();
-
             for (int feet = 1; feet <= 10; feet++) {
-                double meter = toMeter.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
         }
 
         //メートルからフィートへの対応表を出力
         private static void PrintMeterToFeetList(int start, int stop) {
-            FeetConverter fromMeter = new FeetConverter();
-
             for (int meter = 1; meter <= 10; meter++) {
-                double feet = fromMeter.FromMeter(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
         }
