e24ca345bee46084a8e9de8ba0b5201433a325aa
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-05-17 11:27:29 +0900

    Exercise01完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+        //2.1.1
+        //歌のタイトル
+        public string Title { get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間、単位は秒
+        public int Length { get; set; }
+
+        //2.1.2
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+    }
+}
