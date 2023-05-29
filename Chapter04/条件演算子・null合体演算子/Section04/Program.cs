f957cafb25585373f71a153a4ac6a5a96961bd07
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-05-29 11:06:54 +0900

    null合体演算子

--- Chapter04/条件演算子・null合体演算子/Section04/Program.cs
+++ Chapter04/条件演算子・null合体演算子/Section04/Program.cs
@@ -14,7 +14,21 @@ namespace Section04 {
 
             //var num = list.Contains(key) ? 1 : 0;  //条件演算子・三項演算子
             //Console.WriteLine(num);
-#endregion
+            #endregion
+
+            #region null合体演算子
+            string code = "12345";
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+            #endregion
+        }
+
+        private static object GetMessage(object code) {
+            return 1644;
+        }
+
+        private static object DefaultMessage() {
+            return "Default Message";
         }
     }
 }
