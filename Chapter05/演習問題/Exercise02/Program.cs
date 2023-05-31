4ee0d2d76fac8d9489011b4aae56792e8f21d015
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-05-31 12:05:42 +0900

    P158　演習問題　5.2　完成

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            Console.Write("数字列:");
+            var num = Console.ReadLine();
+
+            int ret;
+            if (int.TryParse(num, out ret)) {
+                Console.WriteLine("{0:#,#}", ret);
+            }
+            else {
+                Console.WriteLine("数値文字列でありません");
+            }
         }
     }
 }
