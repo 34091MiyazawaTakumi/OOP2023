28d1833d4a98550f22bd43e7ddaae800ed87de4e
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-06-06 10:09:00 +0900

    P159　5.4　NonArray完成

---
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -0,0 +1,35 @@
+﻿#define NonArray
+//#define StringArray
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+
+#if NonArray
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+#elif StringArray
+            string[] lines = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            };
+#endif
+
+#if NonArray
+            var array = line.Split(';','=').ToArray();
+            Console.WriteLine("作家　:" + array[1]);
+            Console.WriteLine("代表作:" + array[3]);
+            Console.WriteLine("誕生年:" + array[5]);
+#elif StringArray
+            
+#endif
+        }
+    }
+}
