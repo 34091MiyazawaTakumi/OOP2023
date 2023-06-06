afc058856ef7639353c8d3ce8e358905d07a4610
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-06-06 10:31:24 +0900

    P159　5.4　StringArray完成

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,8 +1,6 @@
-﻿#define NonArray
-//#define StringArray
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +8,21 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch(); //タイマーインスタンス生成
+            sw.Start(); //タイマースタート
 
 #if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 #elif StringArray
             string[] lines = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
                 "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
                 "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
                 "Novelist=太宰治;BestWork=人間失格;Born=1909",
@@ -28,8 +36,14 @@ namespace Exercise04 {
             Console.WriteLine("代表作:" + array[3]);
             Console.WriteLine("誕生年:" + array[5]);
 #elif StringArray
-            
+            for (int i = 0; i < lines.Length; i++) {
+                var array = lines[i].Split(';', '=').ToArray();
+                Console.WriteLine("作家　:" + array[1]);
+                Console.WriteLine("代表作:" + array[3]);
+                Console.WriteLine("誕生年:" + array[5]);
+            }
 #endif
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.fffff")); //時間表示
         }
     }
 }
