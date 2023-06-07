3d5e5569de1e5b7d72dfd52cb50d3c48a0bcccf2
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-06-07 11:45:23 +0900

    P183 1_2

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -29,7 +29,9 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2(int[] numbers) {
-            
+            foreach (var num in numbers.Skip(numbers.Length - 2)) {
+                Console.WriteLine(num);
+            }
         }
 
         private static void Exercise1_3(int[] numbers) {
