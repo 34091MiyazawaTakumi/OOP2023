82d64730a659c86f0062d5235afe3535bad95c0c
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-07-03 14:13:59 +0900

    P219　演習問題8.2　完成

---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var day = DateTime.Now;
+
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
+                var nextDay = NextWeek(day, (DayOfWeek)dayofweek);
+                Console.WriteLine("{0}の次週の{1}:{2}",day.ToString("yy/MM/dd"), dayofweek, nextDay.ToString("yy/MM/dd(dddd)"));
+            }
+        }
+
+        public static DateTime NextWeek(DateTime date, DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            if(days <= 7) {
+                days += 7;
+            }
+            return date.AddDays(days);
+        }
+    }
+}
