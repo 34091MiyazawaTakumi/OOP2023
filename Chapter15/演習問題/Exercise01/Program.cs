14f3117f8ee981acce9b7c1327bc5c3912828c42
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-10-12 10:37:27 +0900

    演習問題　15.5　完成

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -34,34 +34,38 @@ namespace Exercise01 {
             var groups = Library.Books.GroupBy(b => b.PublishedYear).OrderBy(g => g.Key);
             foreach (var books in groups) {
                 var book = books.Count();
-                Console.WriteLine("{0}:{1}", books.Key, book);
+                Console.WriteLine("{0}年 {1}冊", books.Key, book);
             }
                 
         }
 
         private static void Exercise1_4() {
             var books = Library.Books.OrderByDescending(b => b.PublishedYear)
-                                        .ThenByDescending(b => b.Price)
-                                        .Join(Library.Categories,
-                                            book => book.CategoryId, category => category.Id,
-                                            (book, category) => new {
-                                                Title = book.Title,
-                                                Category = category.Name,
-                                                Price = book.Price,
-                                                PublishedYear = book.PublishedYear,
-                                            }
-                                        );
+                                     .ThenByDescending(b => b.Price)
+                                     .Join(Library.Categories,
+                                        book => book.CategoryId, category => category.Id,
+                                        (book, category) => new {
+                                            Title = book.Title,
+                                            Category = category.Name,
+                                            Price = book.Price,
+                                            PublishedYear = book.PublishedYear,
+                                        }
+                                     );
 
             foreach (var book in books) {
-                Console.WriteLine("{0}年 {1}円 {2}({3})",
+                Console.WriteLine("{0}年 {1}円 {2} ({3})",
                                 book.PublishedYear, book.Price, book.Title, book.Category);
             }
         }
 
         private static void Exercise1_5() {
-            //foreach (var book in Library.Books.Where(b => b.Publis)) {
-            //    Console.WriteLine("{0}", book);
-            //}
+            foreach (var book in Library.Books.Where(b => b.PublishedYear == 2016)
+                                              .Join(Library.Categories,
+                                                book => book.CategoryId,
+                                                category => category.Id,
+                                                (book, category) => category.Name).Distinct()){
+                Console.WriteLine(book);
+            }
         }
 
         private static void Exercise1_6() {
