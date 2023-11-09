523ea3cf5ef1bf52e1b5dec749a272c1fb76bc21
宮澤 巧 <OIBC34091@ojs.ac.jp>
2023-11-09 10:55:56 +0900

    グラム　ポンド　変換

---
+++ WPF/WeightUnitConverter/ImperialUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightUnitConverter {
+    public class PoundUnit : DistanceUnit {  //ポンド単位を表すクラス
+        private static List<PoundUnit> units = new List<PoundUnit> {
+            new PoundUnit { Name = "oz", Coefficient = 1, },  //オンス
+            new PoundUnit { Name = "lb", Coefficient = 16, },  //ポンド
+        };
+        public static ICollection<PoundUnit> Units { get { return units; } }
+
+        /// <summary>
+        /// グラム単位からポンド単位に変換します
+        /// </summary>
+        /// <param name="unit">グラム単位</param>
+        /// <param name="value">値</param>
+        /// <returns>変換値</returns>
+        public double FromGramUnit(GramUnit unit, double value){
+            return (value * unit.Coefficient) / 28.3495231 / this.Coefficient;
+        }
+    }
+}
---
+++ WPF/WeightUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WeightUnitConverter {
+    public class MainWindowViewModel : ViewModel {
+        private double gramValue, poundValue;
+
+        //▲ボタンで呼ばれるコマンド
+        public ICommand PoundUnitToGram { get; private set; }
+
+        //▼ボタンで呼ばれるコマンド
+        public ICommand GramToPoundUnit { get; private set; }
+
+        //上のComboBoxで選択されている値
+        public GramUnit CurrentGramUnit { get; set; }
+
+        //下のComboBoxで選択されている値
+        public PoundUnit CurrentPoundUnit { get; set; }
+
+        //プロパティ
+        public double GramValue {
+            get { return this.gramValue; }
+            set {
+                this.gramValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public double PoundValue {
+            get { return this.poundValue; }
+            set {
+                this.poundValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel() {
+            this.CurrentGramUnit = GramUnit.Units.First();
+            this.CurrentPoundUnit = PoundUnit.Units.First();
+
+            this.GramToPoundUnit = new DelegateCommand(
+                () => this.PoundValue = this.CurrentPoundUnit.FromGramUnit(
+                    this.CurrentGramUnit, this.GramValue));
+
+            this.PoundUnitToGram = new DelegateCommand(
+                () => this.GramValue = this.CurrentGramUnit.FromPoundUnit(
+                    this.CurrentPoundUnit, this.PoundValue));
+        }
+    }
+}
